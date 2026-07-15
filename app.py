661a02400a9499f93796a1387dd5a467a45ec82d
iziwarehouse43-spec <iziwarehouse43@gmail.com>
2026-07-15 11:18:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-
-from flask import Flask
+from flask import Flask, request, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route("/")
 def home():
-    return {
-        "message": "Courier SLA API is Running",
-        "status": "success"
-    }
+    return jsonify({
+        "message": "Courier SLA API Running"
+    })
 
-@app.route("/health")
-def health():
-    return {
-        "status": "ok"
-    }
+@app.route("/upload", methods=["POST"])
+def upload_excel():
+
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files["file"]
+
+    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(filepath)
+
+    df = pd.read_excel(filepath)
+
+    return jsonify({
+        "rows": len(df),
+        "columns": list(df.columns),
+        "message": "Excel Uploaded Successfully"
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
