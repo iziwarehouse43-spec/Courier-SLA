bf01be8d1af0f4fd4dba6190d02ec0dd5e29924e
iziwarehouse43-spec <iziwarehouse43@gmail.com>
2026-07-15 11:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, request, jsonify
-import pandas as pd
-import os
-
-app = Flask(__name__)
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-@app.route("/")
-def home():
-    return """
-    <h2>Courier SLA API</h2>
-
-    <form action="/upload" method="POST" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <br><br>
-        <button type="submit">Upload Excel</button>
-    </form>
-    """
-
 @app.route("/upload", methods=["POST"])
 def upload():
 
     if "file" not in request.files:
         return jsonify({"error":"No file selected"})
 
-    file = request.files["file"]
+    file=request.files["file"]
 
     filepath=os.path.join(UPLOAD_FOLDER,file.filename)
 
@@ -33,11 +12,35 @@ def upload():
 
     df=pd.read_excel(filepath)
 
-    return jsonify({
-        "message":"File Uploaded Successfully",
-        "Rows":len(df),
-        "Columns":list(df.columns)
-    })
+    df["Dispatch Date"]=pd.to_datetime(df["Dispatch Date"])
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    today=pd.Timestamp.today()
+
+    df["Ageing Days"]=(today-df["Dispatch Date"]).dt.days
+
+
+    courier_sla={
+        "BlueDart":3,
+        "DTDC":5,
+        "Shree Maruti":4
+    }
+
+
+    df["SLA Days"]=df["Shipping Courier"].map(courier_sla)
+
+    df["SLA Status"]=df.apply(
+        lambda x:
+        "BREACH" if x["Ageing Days"]>x["SLA Days"]
+        else "Within SLA",
+        axis=1
+    )
+
+
+    result={
+        "Total Orders":len(df),
+        "SLA Breach":len(df[df["SLA Status"]=="BREACH"]),
+        "Within SLA":len(df[df["SLA Status"]=="Within SLA"])
+    }
+
+
+    return jsonify(result)
