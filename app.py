caeaa36dc19f4f57d61556189cac446d5076e453
iziwarehouse43-spec <iziwarehouse43@gmail.com>
2026-07-15 10:52:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "message": "Courier SLA API is Running",
+        "status": "success"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
